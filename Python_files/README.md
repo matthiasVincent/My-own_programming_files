a5c1aa3130ba31124111e58328aab269e212141f
Matthias Sunday Oduh <sundayoduhmatthias.com>
2022-10-28 13:01:24 -0700

    First Commit

---
+++ Python_files/README.md
@@ -0,0 +1,1 @@
+This housed my online tutorials Python exercises and files
---
+++ Python_files/sum_sample.py
@@ -0,0 +1,11 @@
+import re
+
+fh = open('regexp.txt')
+lis = [ ]
+for line in fh:
+    line = line.rstrip()
+    w = re.findall('[0-9]+', line)
+    if len(w) != 0:
+        for i in w:
+            lis.append(int(i))
+print(sum(lis))
